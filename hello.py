6cab4fa2652a957a039b31b699696ed9e9b46ada
Chen Rui <chenrui666ok@126.com>
2022-03-28 11:26:04 +0800

    added hello.py and welcome.py

--- hello.py
+++ hello.py
@@ -1,8 +1,1 @@
 print("Hello World")
-print("Welcme to the new World")
-
-num = input("Enter num")
-if not(int(num) % 2):
-    print("Even")
-else:
-    print("odd")
--- welcome.py
+++ welcome.py
@@ -1,7 +1,1 @@
 print("Welcme to the new World")
-
-num = input("Enter num")
-if not(int(num) % 2):
-    print("evennnnnnn")
-else:
-    print("oddddd")
